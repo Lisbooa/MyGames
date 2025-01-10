b7976c6c1d500d95e37bd9f932a9ea0f336f49c3
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-01-10 14:56:07 -0300

    Project created and add images

---
+++ MyGames/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace MyGames.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string DetailedDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool Available { get; set; }
+
+        public int GenderId { get; set; }
+        public virtual Gender Gender { get; set; }
+    }
+}
---
+++ MyGames/Models/Gender.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGames.Models
+{
+    public class Gender
+    {
+        public int GenderId { get; set; }
+        public string GenderName { get; set; }
+        public string Description { get; set; }
+
+        public List<Game> Games { get; set; }
+    }
+}
