668c696a189b152475b06e2d5068a5978db29557
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-01-14 11:53:48 -0300

    Add Data Annotations

--- MyGames/Models/Game.cs
+++ MyGames/Models/Game.cs
@@ -1,14 +1,52 @@
-﻿namespace MyGames.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyGames.Models
 {
+    [Table("Games")]
     public class Game
     {
+        [Key]
         public int GameId { get; set; }
+
+        [Required(ErrorMessage = "O nome do jogo deve ser informado")]
+        [Display(Name = "Nome do jogo")]
+        [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "A descrição do jogo deve ser informada")]
+        [Display(Name = "Descrição do jogo")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
         public string ShortDescription { get; set; }
+
+
+        [Required(ErrorMessage = "A descrição detalhada do jogo deve ser informada")]
+        [Display(Name = "Descrição detalhada do jogo")]
+        [MinLength(20, ErrorMessage = "Descrição detalhada deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição detalhada não pode exceder {1} caracteres")]
         public string DetailedDescription { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o preço do jogo")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage = "O preço deve estar entre 1 e 999,99")]
         public decimal Price { get; set; }
+
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImageUrl { get; set; }
+
+
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
         public string ImageThumbnailUrl { get; set; }
+
+
+        [Display(Name = "Estoque")]
         public bool Available { get; set; }
 
         public int GenderId { get; set; }
--- MyGames/Models/Gender.cs
+++ MyGames/Models/Gender.cs
@@ -1,9 +1,22 @@
-﻿namespace MyGames.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyGames.Models
 {
+    [Table("Genders")]
     public class Gender
     {
+        [Key]
         public int GenderId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do gênero")]
+        [Display(Name = "Nome")]
         public string GenderName { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição do gênero")]
+        [Display(Name = "Descrição")]
         public string Description { get; set; }
 
         public List<Game> Games { get; set; }
