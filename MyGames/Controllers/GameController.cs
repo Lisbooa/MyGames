58ae4722133b65f2905f3fefd8fd38a48040f3e8
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-01-17 13:01:59 -0300

    Created GameController and View List

---
+++ MyGames/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGames.Repositories.Interfaces;
+
+namespace MyGames.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public GameController(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public IActionResult List()
+        {
+            var games = _gameRepository.Games;
+            return View(games);
+        }
+    }
+}
