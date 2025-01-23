dbae81e1329d960fad506cc0da92c964ea279414
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-01-23 15:47:19 -0300

    Configured Startup to using MemoryCache, Session and HttpContexAcessor

--- MyGames/Startup.cs
+++ MyGames/Startup.cs
@@ -20,7 +20,12 @@ public class Startup
         services.AddTransient<IGameRepository, GameRepository>();
         services.AddTransient<IGenderRepository, GenderRepository>();
 
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +45,8 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
+
+        app.UseSession();
 
         app.UseAuthorization();
 
