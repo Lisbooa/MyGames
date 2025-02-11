33d1d58aecbee72549f1ab5bf6c8a44696530f41
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-02-11 15:38:12 -0300

    Created a ViewComponent ShopCartResume, Add a icon to Navbar with a count of ShopCartItems

---
+++ MyGames/Components/ShopCartResume.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGames.Models;
+using MyGames.ViewModels;
+
+namespace MyGames.Components
+{
+    public class ShopCartResume : ViewComponent
+    {
+        private readonly ShopCart _shopCart;
+
+        public ShopCartResume(ShopCart shopCart)
+        {
+            _shopCart = shopCart;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var items = _shopCart.GetShopCartItems();
+            //var items = new List<ShopCartItem>() 
+            //{
+            //    new ShopCartItem(),
+            //    new ShopCartItem()
+            //};
+            _shopCart.ShopCartItems = items;
+
+            var shopCartVM = new ShopCartViewModel
+            {
+                ShopCart = _shopCart,
+                ShopCartTotal = _shopCart.GetShopCartTotal()
+            };
+            return View(shopCartVM);
+        } 
+    }
+}
