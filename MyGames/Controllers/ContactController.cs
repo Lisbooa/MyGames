ee74a6ff17c85ec50b37f2377d3ed56c9f9b056e
Laeverton Lisboa <joselaevertonlisboa@hotmail.com>
2025-02-17 16:02:51 -0300

    Created TagHelper Email, created Controller, View e Add to View Imports and Layout

---
+++ MyGames/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyGames.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
